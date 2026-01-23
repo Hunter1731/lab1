f3f482bf919954dd443ad43c455b9148476314c5
Hunter1731 <mfbfhpetyx@gmail.com>
2026-01-23 12:33:57 +0200

    Fix file path output in C++ program

--- 3.2 copy 2.cpp
+++ 3.2 copy 2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
-    std::cout << "Шлях до файлу: " << "C:\Games\MyGame" << "\n" << "Пункт 1     Пункт 2     Пункт 3"
+    std::cout << "Шлях до файлу: " << "\"C:\\Games\\MyGame\"" << "\n" << "Пункт 1     Пункт 2     Пункт 3"
     << std::endl;
     return 0;
     }
